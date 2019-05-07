e41f668b61c5fa2509104be27383b070e1b45e68
fadila08 <nurulfadila08@gmail.com>
2019-05-07 23:58:24 +0700

    detail pengumuman on laboran

--- app/src/main/java/untag/daskom/myapplication/adapter/laboran/LABORAN_PengumumanAdapter.java
+++ app/src/main/java/untag/daskom/myapplication/adapter/laboran/LABORAN_PengumumanAdapter.java
@@ -1,7 +1,6 @@
 package untag.daskom.myapplication.adapter.laboran;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +9,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-import untag.daskom.myapplication.LABORANDetilPengumuman;
+import untag.daskom.myapplication.activity.laboran.LABORANDetilPengumuman;
 import untag.daskom.myapplication.R;
 import untag.daskom.myapplication.model.PengumumanModel;
 
