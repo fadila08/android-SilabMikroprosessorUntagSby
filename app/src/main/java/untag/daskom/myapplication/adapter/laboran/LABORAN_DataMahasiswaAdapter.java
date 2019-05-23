5407d8c8ddd29a85b42535fbe1cfc87cee5b3785
fadila08 <nurulfadila08@gmail.com>
2019-05-23 10:28:49 +0700

    read mhs on laboran

--- app/src/main/java/untag/daskom/myapplication/adapter/laboran/LABORAN_DataMahasiswaAdapter.java
+++ app/src/main/java/untag/daskom/myapplication/adapter/laboran/LABORAN_DataMahasiswaAdapter.java
@@ -1,5 +1,6 @@
 package untag.daskom.myapplication.adapter.laboran;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,15 +8,19 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import untag.daskom.myapplication.R;
+import untag.daskom.myapplication.model.DataMahasiswa;
 import untag.daskom.myapplication.model.DataUser;
 
 public class LABORAN_DataMahasiswaAdapter extends RecyclerView.Adapter<LABORAN_DataMahasiswaAdapter.DataMahasiswaViewHolder>  {
-    private ArrayList<DataUser> dataList;
+    private List<DataMahasiswa> dataList;
+    Context context;
 
-    public LABORAN_DataMahasiswaAdapter(ArrayList<DataUser> dataList) {
+    public LABORAN_DataMahasiswaAdapter(List<DataMahasiswa> dataList, Context context) {
         this.dataList = dataList;
+        this.context = context;
     }
 
     @Override
@@ -27,8 +32,9 @@ public class LABORAN_DataMahasiswaAdapter extends RecyclerView.Adapter<LABORAN_D
 
     @Override
     public void onBindViewHolder(DataMahasiswaViewHolder holder, int position) {
-        holder.txtNama.setText(dataList.get(position).getNama());
-        holder.txtNomorInduk.setText(dataList.get(position).getNomor_induk());
+        holder.txtNama.setText(dataList.get(position).getNama_mahasiswa());
+        holder.txtNomorInduk.setText(dataList.get(position).getNbi_mahasiswa());
+        holder.txtId.setText(dataList.get(position).getId());
     }
 
 
@@ -39,12 +45,13 @@ public class LABORAN_DataMahasiswaAdapter extends RecyclerView.Adapter<LABORAN_D
 
     class DataMahasiswaViewHolder extends RecyclerView.ViewHolder {
 
-        TextView txtNama, txtNomorInduk;
+        TextView txtNama, txtNomorInduk, txtId;
 
         DataMahasiswaViewHolder(View itemView) {
             super(itemView);
             txtNama = itemView.findViewById(R.id.txt_nama_mahasiswa_laboran);
             txtNomorInduk = itemView.findViewById(R.id.txt_nomor_induk_mahasiswa_laboran);
+            txtId = itemView.findViewById(R.id.txt_id_mahasiswa_laboran);
         }
     }
 
